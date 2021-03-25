6ebd6c30ccf048bb29d1b243966e19502b364153
loic-roux <69loloro10@gmail.com>
2021-03-25 15:19:12 +0100

    chore: build tools script win

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -2,5 +2,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # tooling
 choco install -y cygwin
-choco install -y make
-choco install -y --force llvm
+choco install -y msys2
+refreshenv
+pacman -Ss make
+pacman -Ss findutils
+pacman -S 'mingw-w64-x86_64-llvm'
