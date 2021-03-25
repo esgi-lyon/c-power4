fcbd2b3cf1c0b942a05ecfac45f113b4cb128ca8
loic-roux <69loloro10@gmail.com>
2021-03-25 23:55:47 +0100

    feat: diagonale part 1/4

--- game/level.c
+++ game/level.c
@@ -5,6 +5,7 @@
 #include "grid/displayer.h"
 #include "grid/controller.h"
 #include "colors.h"
+#include <math.h>
 
 GridMap game_grid;
 int win_status = 0;
@@ -28,30 +29,47 @@ void switch_player() {
   }
 }
 
-enum Case* reset_or_win(int* line, enum Case* previous, enum Case player) {
-  if (*line == 4) {
+enum Case* is_win(int line, enum Case player) {
+  if (line == 3) {
     win_status = 1;
     const char separator[] = "=======================";
-    printf(KNRM "\n %s \n" RESET, separator);
-    printf(KBLU " ==== %s WINNED ====" RESET, get_case_char(player));
-    printf(KNRM "\n %s \n" RESET, separator);
+    printf(KBLU "\n %s \n", separator);
+    printf(" ==== %s WINNED ====", get_case_char(player));
+    printf("\n %s \n" RESET, separator);
 
     return (enum Case*) player;
   }
 
-  line = 0;
-  previous = NULL;
-
   return NULL;
 }
 
-void calculate_win(GridMap* grid, enum Case player) {
-  // line vertical
+int win_vertical(GridMap* grid, enum Case player) {
+  int x, y;
+  int line = 0;
+  enum Case previous;
+
+  for (x = 0; x < X_SIZE; x++) {
+    for (y = Y_SIZE - 1; y >= 0; y--) {
+      if (
+        player == grid->matrix[x][y] &&
+        grid->matrix[x][y] == previous
+      ) {
+        line++;
+      }
+
+      previous = grid->matrix[x][y];
+    }
+  }
+
+  return line;
+}
+
+int win_horizontal(GridMap* grid, enum Case player) {
   int x, y;
   enum Case previous;
-  int line = 1;
+  int line = 0;
 
-  // horizontal
+  // line horizontal
   for (y = Y_SIZE - 1; y >= 0; y--) {
     for (x = 0; x < X_SIZE; x++) {
       if (
@@ -65,31 +83,79 @@ void calculate_win(GridMap* grid, enum Case player) {
     }
   }
 
-  if (reset_or_win(&line, &previous, player) != NULL) {
-    return;
-  }
+  return line;
+}
 
-  // vertical
-  for (x = 0; x < X_SIZE; x++) {
-    for (y = Y_SIZE - 1; y >= 0; y--) {
-      if (
-        player == grid->matrix[x][y] &&
-        grid->matrix[x][y] == previous
-      ) {
-        line++;
-      }
-      previous = grid->matrix[x][y];
+int win_diagonal_bottom(GridMap* grid, enum Case player) {
+  int x, y;
+  x = y = 0;
+  enum Case previous;
+  int line = 0;
+  if (player == grid->matrix[x][y]) previous = grid->matrix[x][y];
+
+  while (y < Y_SIZE || x < (int) round(X_SIZE / 2) + 1) {
+    y++;
+    x++;
+
+    if (
+      player == grid->matrix[x][y] &&
+      previous == grid->matrix[x][y]
+    ) {
+      line++;
     }
+
+    previous = grid->matrix[x][y];
   }
 
-  if (reset_or_win(&line, &previous, player)) {
+  return line;
+}
+
+int win_diagonal_bottom_reverse(GridMap* grid, enum Case player) {
+  int x, y;
+  y = 0;
+  x = X_SIZE;
+  enum Case previous;
+  int line = 0;
+
+  if (player == grid->matrix[x - 1][y])
+    previous = grid->matrix[x -1][y];
+
+  while (y < Y_SIZE || x > 0) {
+    y++;
+    x--;
+    printf("x : %d / y : %d", x, y);
+
+    if (
+      player == grid->matrix[x][y] &&
+      previous == grid->matrix[x][y]
+    ) {
+      printf("%s", get_case_char(grid->matrix[x][y]));
+      line++;
+    }
+    printf("\n");
+
+    previous = grid->matrix[x][y];
+  }
+  printf("\n\n");
+  return line;
+}
+
+void calculate_win(GridMap* grid, enum Case player) {
+  if (is_win(win_horizontal(grid, player), player) != NULL) {
     return;
   }
 
-  // diagonal
-  // TODO
+  if (is_win(win_vertical(grid, player), player) != NULL) {
+    return;
+  }
 
-  return;
+  if (is_win(win_diagonal_bottom(grid, player), player) != NULL) {
+    return;
+  }
+
+  if (is_win(win_diagonal_bottom_reverse(grid, player), player) != NULL) {
+    return;
+  }
 }
 
 int determine_y_in_col(GridMap* grid, unsigned int x) {
